35c9db967d941130c78ca9fcd7052f27f3cd9759
rodiak <rody@raknet.me>
2022-04-18 19:44:52 +0300

    General bugfixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,26 @@
-const profileEditBtn = document.querySelector(".profile__edit-btn");
 const profileNameTxt = document.querySelector(".profile__name-title");
 const profileDescTxt = document.querySelector(".profile__description");
-const addButton = document.querySelector(".profile__add-btn");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const addPlaceBtn = document.querySelector(".profile__add-btn");
 
-const editPopup = document.getElementById("edit-popup");
+const editProfilePopup = document.getElementById("edit-popup");
 const editFormElement = document.getElementById("edit-form");
-const editFormInputs = editFormElement.querySelectorAll(".popup__input");
+const editProfileCloseBtn = editProfilePopup.querySelector(".popup__close-btn");
 
-const addPopup = document.getElementById("add-popup");
+const addPlacePopup = document.getElementById("add-popup");
 const addFormElement = document.getElementById("add-form");
 const addFormInputs = addFormElement.querySelectorAll(".popup__input");
+const addPlaceCloseBtn = addPlacePopup.querySelector(".popup__close-btn");
+
+const cardTemplate = document.querySelector("#card-template").content;
 
 const photoPreviewPopup = document.getElementById("photo-viewer-popup");
-
-const popupCloseBtn = document.querySelectorAll(".popup__close-btn");
+const photoPreviewCloseBtn =
+  photoPreviewPopup.querySelector(".popup__close-btn");
 
 const gallery = document.querySelector(".gallery__list");
 
 function createCard(cardData) {
-  const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardPhoto = cardElement.querySelector(".card__photo-container");
   const cardTitle = cardElement.querySelector(".card__title");
@@ -51,46 +53,54 @@ function initiateGallery() {
   });
 }
 
-function openPopup(popupID) {
-  popupID.classList.add("popup__active");
+function openPopup(popup) {
+  popup.classList.add("popup__active");
 }
 
-function closePopup(evt) {
-  evt.target.closest(".popup__active").classList.remove("popup__active");
+function closePopup(popup) {
+  popup.classList.remove("popup__active");
 }
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileNameTxt.textContent = editFormInputs[0].value;
-  profileDescTxt.textContent = editFormInputs[1].value;
-  closePopup(evt);
+  profileNameTxt.textContent = editFormElement["name"].value;
+  profileDescTxt.textContent = editFormElement["description"].value;
+  closePopup(editProfilePopup);
 }
 
 function handleAddFormSubmit(evt) {
   evt.preventDefault();
   const newCard = {
-    name: addFormInputs[0].value,
-    link: addFormInputs[1].value,
+    name: addFormElement["title"].value,
+    link: addFormElement["link"].value,
   };
   gallery.prepend(createCard(newCard));
-  closePopup(evt);
+  closePopup(addPlacePopup);
 }
 
 initiateGallery();
 
-profileEditBtn.addEventListener("click", function () {
-  editFormInputs[0].value = profileNameTxt.textContent;
-  editFormInputs[1].value = profileDescTxt.textContent;
-  openPopup(editPopup);
+editProfileBtn.addEventListener("click", function () {
+  editFormElement["name"].value = profileNameTxt.textContent;
+  editFormElement["description"].value = profileDescTxt.textContent;
+  openPopup(editProfilePopup);
 });
 
-addButton.addEventListener("click", function () {
+addPlaceBtn.addEventListener("click", function () {
   addFormElement.reset();
-  openPopup(addPopup);
+  openPopup(addPlacePopup);
 });
 
-popupCloseBtn.forEach((item) => {
-  item.addEventListener("click", closePopup);
+editProfileCloseBtn.addEventListener("click", function () {
+  closePopup(editProfilePopup);
+});
+
+addPlaceCloseBtn.addEventListener("click", function () {
+  closePopup(addPlacePopup);
+});
+
+photoPreviewCloseBtn.addEventListener("click", function () {
+  closePopup(photoPreviewPopup);
 });
 
 editFormElement.addEventListener("submit", handleProfileFormSubmit);
